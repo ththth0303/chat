a9bae62c46371293917fbb14faa45f8d489cd058
ngo.trung.thang <ngo.trung.thang@framgia.com>
2017-10-10 15:27:22 +0700

    iniy

---
+++ server/server.js
@@ -0,0 +1,11 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+app.use(express.static(path.join(__dirname,"../app/dist")));
+app.get('/1', function (req, res) {
+  res.render('index');
+});
+app.listen(7777,function(){
+    console.log("Started listening on port", 7777);
+})
